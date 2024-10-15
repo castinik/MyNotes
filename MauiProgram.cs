e841f8d7a964db2c09699ffe7a1d9c10d01b3a31
Castinik <nicolo110@live.it>
2024-10-15 12:55:32 +0200

    StatusBarBehavior

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using MyNotes.Pages;
 using MyNotes.Services;
 using MyNotes.ViewModels;
@@ -12,6 +13,7 @@ namespace MyNotes
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
